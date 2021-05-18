1a828ef94815f4d73b5ee3b3a0dc9bdac570fcac
Hudson Ayers <hayers@stanford.edu>
2021-05-18 12:48:54 -0700

    updates

--- conf/env.sh
+++ conf/env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Change these values depending on your setup
-GLINT_JAR_PATH=./bin/Glint-0.1.jar   # Path to the assembled glint jar file, this should be the same on all cluster machines
-GLINT_MASTER_OPTS="-Xmx2048m"        # Java options to pass the JVM when starting a master
-GLINT_SERVER_OPTS="-Xmx2048m"        # Java options to pass the JVM when starting a server
+GLINT_JAR_PATH=/home/ec2-user/Glint-assembly-0.1-SNAPSHOT.jar   # Path to the assembled glint jar file, this should be the same on all cluster machines
+GLINT_MASTER_OPTS="-Xmx16384m"        # Java options to pass the JVM when starting a master
+GLINT_SERVER_OPTS="-Xmx16384m"        # Java options to pass the JVM when starting a server
